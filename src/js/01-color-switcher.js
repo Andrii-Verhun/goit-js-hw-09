5410b8ca34768725193cd40b0139b6b4bedb95cd
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-24 09:39:32 +0200

    Task 1 complete

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startButton = document.querySelector('[data-start]');
+const stopButton = document.querySelector('[data-stop]');
+let timerID = null;
 
+const getRandomHexColor = () => {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const colorSwitcher = () => {
+    const body = document.querySelector('body');
+    if (!startButton.disabled) {
+        startButton.setAttribute('disabled', 'true');
+        stopButton.removeAttribute('disabled');
+        timerID = setInterval(() => {
+            body.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+        return
+    };
+    startButton.removeAttribute('disabled');
+    stopButton.setAttribute('disabled', 'true');
+    clearInterval(timerID);
+};
+
+startButton.addEventListener('click', colorSwitcher);
+stopButton.addEventListener('click', colorSwitcher);
