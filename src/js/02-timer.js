549285ce362f42604f884e2596857c95070d3dea
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-24 11:22:45 +0200

    Task 2 in progress

---
+++ src/js/02-timer.js
@@ -0,0 +1,62 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const startTimer = document.querySelector('[data-start]');
+startTimer.setAttribute('disabled', 'true');
+let timerValueMs = 0;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+      if (selectedDates[0] - Date.now() <= 0) {
+        startTimer.setAttribute('disabled', 'true');
+        window.alert("Please choose a date in the future");
+        };
+
+      if (selectedDates[0] - Date.now() > 0) {
+          startTimer.removeAttribute('disabled');
+          timerValueMs = selectedDates[0] - Date.now();
+        };
+    },
+};
+flatpickr("#datetime-picker", options);
+
+const convertMs = (ms) => {
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    const days = Math.floor(ms / day);
+    const hours = Math.floor((ms % day) / hour);
+    const minutes = Math.floor(((ms % day) % hour) / minute);
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+    return { days, hours, minutes, seconds };
+};
+
+const addLeadingZero = (value) => {
+    value.days = value.days.toString().padStart(2, '0');
+    value.hours = value.hours.toString().padStart(2, '0');
+    value.minutes = value.minutes.toString().padStart(2, '0');
+    value.seconds = value.seconds.toString().padStart(2, '0');
+};
+
+const startingTimer = (timerValue) => {
+    setInterval(() => {
+        const time = convertMs(timerValue);
+        addLeadingZero(time);
+        console.log(time.minutes);
+        console.log(time);
+        timerValue -= 1000;
+
+    }, 1000);
+
+};
+
+startTimer.addEventListener('click', () => {
+    startingTimer(timerValueMs);
+});
