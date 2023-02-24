eeb161a5a75ee9c6b4e518e3802170d5396dd5af
Andrii-Verhun <113340615+Andrii-Verhun@users.noreply.github.com>
2023-02-24 14:25:46 +0200

    Task 3 complete

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,43 @@
+import Notiflix from 'notiflix';
+import "notiflix/dist/notiflix-3.2.6.min.css";
+
+const formGenPromise = document.querySelector('.form');
+
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  const promise = new Promise((res, rej) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        res({ position, delay });
+      } else {
+        rej({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise.then(({ position, delay }) => {
+    Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
+      useIcon: false,
+    });
+  })
+  .catch(({ position, delay }) => {
+    Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, {
+      useIcon: false,
+    });
+  });
+  return promise;
+};
+
+formGenPromise.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const {
+    elements: {delay, step, amount} 
+  } = e.currentTarget;
+  let delayValue = parseInt(delay.value, 10);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delayValue);
+    delayValue += parseInt(step.value, 10);
+  };
+});
